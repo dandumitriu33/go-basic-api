b65d476b022c54ec7b6f5a94fda92429df49f3b6
Dan Dumitriu <dandumitriu33@gmail.com>
2022-06-18 16:22:33 +0300

    Add PATCH checkout book by id param

--- main.go
+++ main.go
@@ -46,6 +46,27 @@ func bookById(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, book)
 }
 
+func checkoutBook(c *gin.Context) {
+	// query parameter ?=2
+	id, ok := c.GetQuery("id")
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+	book, err := getBookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available."})
+		return
+	}
+
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func getBookById(id string) (*book, error) {
 	for i, b := range books {
 		if b.ID == id {
@@ -62,5 +83,6 @@ func main() {
 	router.GET("/books", getBooks)
 	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
 	router.Run("localhost:8080")
 }
