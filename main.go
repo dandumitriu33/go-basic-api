84d502840bf7c15c56b036cb799473e9a42c2b64
Dan Dumitriu <dandumitriu33@gmail.com>
2022-06-18 15:50:13 +0300

    Add minimal API with GET all

--- main.go
+++ main.go
@@ -3,8 +3,32 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+
 func main() {
-	log.Println("Hello world.")
+	log.Println("API started.")
+
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
 }
