4aa5009e1ee07b663bc54b5d5533862267e48b1a
Dan Dumitriu <dandumitriu33@gmail.com>
2022-06-18 16:01:06 +0300

    Add POST create book

--- main.go
+++ main.go
@@ -25,10 +25,21 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func createBook(c *gin.Context) {
+	var newBook book
+	if err := c.BindJSON(&newBook); err != nil {
+		// BindJSON handles the error
+		return
+	}
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main() {
 	log.Println("API started.")
 
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
